cdbf6a90f4ed4a6e04ca12909e9e4be9af26a66c
Cong <xiaohanzi1001@gmail.com>
2017-06-11 12:57:05 +0200

    selfname model website address method 3

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,10 @@ class Product < ApplicationRecord
 
   before_validation :generate_friendly_id, :on => :create
 
+  validates_presence_of :name, :friendly_id
+  validates_uniqueness_of :friendly_id
+  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]\z/
+
    def to_param
      self.friendly_id
    end
